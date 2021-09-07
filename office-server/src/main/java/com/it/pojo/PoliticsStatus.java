0049c14b8ef0d7de7ad5b14e1dc283a554e6e143
luyuanxyw <1451773921@qq.com>
2021-09-07 09:13:58 +0800

    新增员工管理，导入导出员工数据

--- office-server/src/main/java/com/it/pojo/PoliticsStatus.java
+++ office-server/src/main/java/com/it/pojo/PoliticsStatus.java
@@ -1,14 +1,14 @@
 package com.it.pojo;
-
-import com.baomidou.mybatisplus.annotation.TableName;
+import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 
 /**
  * <p>
@@ -19,7 +19,9 @@ import lombok.experimental.Accessors;
  * @since 2021-08-22
  */
 @Data
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode(callSuper = false , of = "name")
+@RequiredArgsConstructor
+@NoArgsConstructor
 @Accessors(chain = true)
 @TableName("t_politics_status")
 @ApiModel(value="PoliticsStatus对象", description="")
@@ -32,6 +34,8 @@ public class PoliticsStatus implements Serializable {
     private Integer id;
 
     @ApiModelProperty(value = "政治面貌")
+    @Excel(name = "政治面貌",width = 15)
+    @NonNull
     private String name;
 
 
